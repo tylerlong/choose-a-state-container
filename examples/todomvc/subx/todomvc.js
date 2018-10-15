320ac0aa3a6f55ea46ab42204a263c6e2b64cf23
Tyler Long <tyler4long@gmail.com>
2018-10-15 18:51:29 +0800

    Fix store bind bug of the subx todomvc demo

--- examples/todomvc/subx/todomvc.js
+++ examples/todomvc/subx/todomvc.js
@@ -145,21 +145,22 @@ class TodoItem extends ReactSubX.Component {
 // component Footer
 class Footer extends ReactSubX.Component {
   render () {
-    const { length, leftCount, visibility, doneCount, clearCompleted } = this.props.store
-    if (length === 0) {
+    const { store } = this.props
+    if (store.length === 0) {
       return ''
     }
     return <footer className='footer'>
       <span className='todo-count'>
-        <strong>{pluralize('item', leftCount, true)}</strong> left
+        <strong>{pluralize('item', store.leftCount, true)}</strong> left
       </span>
       <ul className='filters'>
-        <li><a href='#/all' className={classNames({ selected: visibility === 'all' })}>All</a></li>
-        <li><a href='#/active' className={classNames({ selected: visibility === 'active' })}>Active</a></li>
-        <li><a href='#/completed' className={classNames({ selected: visibility === 'completed' })}>Completed</a></li>
+        <li><a href='#/all' className={classNames({ selected: store.visibility === 'all' })}>All</a></li>
+        <li><a href='#/active' className={classNames({ selected: store.visibility === 'active' })}>Active</a></li>
+        <li><a href='#/completed' className={classNames({ selected: store.visibility === 'completed' })}>Completed</a>
+        </li>
       </ul>
-      {doneCount <= 0 ? ''
-        : <button className='clear-completed' onClick={e => clearCompleted()}>Clear completed</button>}
+      {store.doneCount <= 0 ? ''
+        : <button className='clear-completed' onClick={e => store.clearCompleted()}>Clear completed</button>}
     </footer>
   }
 }
