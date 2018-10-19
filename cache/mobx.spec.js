e8dafb8a84de8906a868c56bf2498bebf03ce374
Tyler Long <tyler4long@gmail.com>
2018-10-19 22:20:52 +0800

    Finish mobx cache test case

--- cache/mobx.spec.js
+++ cache/mobx.spec.js
@@ -1,20 +1,18 @@
 /* eslint-env jest */
 import { observable, autorun } from 'mobx'
 
-let count = 0
-const calculator = observable.object({
-  a: 1,
-  b: 2,
-  get add () {
-    count += 1
-    return this.a + this.b
-  }
-})
-
-autorun(() => calculator.add) // just to add a observer
-
 describe('Cache - MobX', () => {
-  test('default', () => {
+  test('with observer', () => {
+    let count = 0
+    const calculator = observable.object({
+      a: 1,
+      b: 2,
+      get add () {
+        count += 1
+        return this.a + this.b
+      }
+    })
+    autorun(() => calculator.add) // just to add a observer
     expect(calculator.add).toBe(3)
     expect(calculator.add).toBe(3)
     expect(count).toBe(1)
@@ -23,6 +21,27 @@ describe('Cache - MobX', () => {
     calculator.a = 4
     expect(calculator.add).toBe(6)
     expect(calculator.add).toBe(6)
-    expect(count).toBe(4) // Oh no! count should be 2! So it re-compute every time because there is an observer? Not want I want
+    expect(count).toBe(4) // Oh no! It re-computes every time because there is an observer?!
+  })
+
+  test('without observer', () => {
+    let count = 0
+    const calculator = observable.object({
+      a: 1,
+      b: 2,
+      get add () {
+        count += 1
+        return this.a + this.b
+      }
+    })
+    expect(calculator.add).toBe(3)
+    expect(calculator.add).toBe(3)
+    expect(count).toBe(2) // no cache, sadly
+    calculator.a = 2
+    calculator.a = 3
+    calculator.a = 4
+    expect(calculator.add).toBe(6)
+    expect(calculator.add).toBe(6)
+    expect(count).toBe(4) // no cache, sadly
   })
 })
