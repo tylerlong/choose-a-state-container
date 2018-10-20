e627684739a686acaed59e28b5db6fad5cb861c4
Tyler Long <tyler4long@gmail.com>
2018-10-20 19:01:25 +0800

    Add experiments folder

---
+++ experiments/README.md
@@ -0,0 +1,6 @@
+# Experiments
+
+This folder contains experiments.
+
+Those experiments are mostly for learning and confirming purpose.
+Whenever I am not sure how a specific aspect of a state container behaves, I create an experiment here to try it out.
--- experiments/mobx_decorator.spec.js
+++ experiments/mobx_decorator.spec.js
@@ -55,4 +55,56 @@ describe('MobX decorator', () => {
     expect(calculator.sum).toBe(4)
     expect(count).toBe(2)
   })
+
+  test('@decorator + keepAlive', () => {
+    let count = 0
+    class Calculator {
+      @observable a = 1
+      @observable b = 2
+      @computed({ keepAlive: true }) get sum () {
+        count += 1
+        return this.a + this.b
+      }
+      @action changeA (a) {
+        this.a = a
+      }
+    }
+    const calculator = new Calculator()
+    expect(calculator.sum).toBe(3)
+    expect(calculator.sum).toBe(3)
+    expect(calculator.sum).toBe(3)
+    expect(count).toBe(1)
+    calculator.changeA(2)
+    expect(calculator.sum).toBe(4)
+    expect(count).toBe(2)
+  })
+
+  test('decorate() + keepAlive', () => {
+    let count = 0
+    class Calculator {
+      a = 1
+      b = 2
+      get sum () {
+        count += 1
+        return this.a + this.b
+      }
+      changeA (a) {
+        this.a = a
+      }
+    }
+    decorate(Calculator, {
+      a: observable,
+      b: observable,
+      sum: computed({ keepAlive: true }),
+      changeA: action
+    })
+    const calculator = new Calculator()
+    expect(calculator.sum).toBe(3)
+    expect(calculator.sum).toBe(3)
+    expect(calculator.sum).toBe(3)
+    expect(count).toBe(1)
+    calculator.changeA(2)
+    expect(calculator.sum).toBe(4)
+    expect(count).toBe(2)
+  })
 })
