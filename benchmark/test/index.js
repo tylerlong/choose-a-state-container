5e52c6a10e14296f6c676eab2057b158c43d59ad
Tyler Long <tyler4long@gmail.com>
2018-10-21 22:09:34 +0800

    Add a test benchmark

---
+++ benchmark/test/index.js
@@ -0,0 +1,26 @@
+import Benchmark from 'benchmark'
+import * as R from 'ramda'
+import uuid from 'uuid/v1'
+
+let a = []
+const suite1 = new Benchmark.Suite()
+suite1
+  .add('array push', () => {
+    R.range(0, 1000).forEach(i => a.push(uuid()))
+    a = []
+  })
+  .add('array shift', () => {
+    R.range(0, 500).forEach(i => a.shift())
+    R.range(0, 500).forEach(i => a.push(uuid()))
+  })
+  .add('array pop', () => {
+    R.range(0, 500).forEach(i => a.pop())
+    R.range(0, 500).forEach(i => a.push(uuid()))
+  })
+  .on('cycle', function (event) {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  .run()
