57d2238af829afa99044b35c721a9f774c850b09
Tyler Long <tyler4long@gmail.com>
2018-10-23 15:41:37 +0800

    Simplify code for React.setState counter

--- examples/counter/setState/counter.js
+++ examples/counter/setState/counter.js
@@ -4,20 +4,12 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = { number: 0 }
-    this.decrease = this.decrease.bind(this)
-    this.increase = this.increase.bind(this)
-  }
-  decrease () {
-    this.setState({ number: this.state.number - 1 })
-  }
-  increase () {
-    this.setState({ number: this.state.number + 1 })
   }
   render () {
     return <div>
-      <button onClick={this.decrease}>-</button>
+      <button onClick={e => this.setState({ number: this.state.number - 1 })}>-</button>
       <span>{this.state.number}</span>
-      <button onClick={this.increase}>+</button>
+      <button onClick={e => this.setState({ number: this.state.number + 1 })}>+</button>
     </div>
   }
 }
